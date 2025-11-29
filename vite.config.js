a795d82b026c2e7f1de3fcbdec0edc33b6837d3b
Rakesh T <rakeshthangaraj89@gmail.com>
2025-11-29 15:13:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@google/genai'] // ✅ Tell Rollup not to bundle this
+    }
+  }
 })
